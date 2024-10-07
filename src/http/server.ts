69987186e282484a38b13614413b8fbe021a7b7f
Fabiano S. Ramos <fabiano.suporteinfo@gmail.com>
2024-10-07 01:44:10 -0300

    Initial config + Docker/Postgres configs

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+    .get('/', () => {
+        return {message: "Hello World"}
+    })
+
+app.listen(3333, () => {
+    console.log("Server running")
+})
