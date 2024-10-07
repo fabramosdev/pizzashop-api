b366a18534b9a9550fe47792320babb2cc6ee6f0
Fabiano S. Ramos <fabiano.suporteinfo@gmail.com>
2024-10-07 12:09:22 -0300

    add drizzle migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
-  dialect: "postgresql"
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://docker:docker@localhost:5432/pizzashop"
+  },
 } satisfies Config;
